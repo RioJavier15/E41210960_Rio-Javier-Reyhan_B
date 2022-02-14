6901d4a97ea6a0d10c5a78dca177c503ce7e2913
E41210960_Rio Javier Reyhan_B <rioreyhanjavier@gmail.com>
2022-02-15 02:06:06 +0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author Rio Javier
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //menggunakan println
+        System.out.println("Hello world");
+        System.out.println("Nama saya Rio Javier Reyhan");
+        
+    //menggunakan print
+        System.out.print("Hello world");
+        System.out.print("Nama saya Rio Javier Reyhan");
+        
+    //menanmpilkan isi variabel panjang
+        int panjang = 10;
+        System.out.println(panjang);
+        
+    //menampilkan kalimat "panjang segi empat" + variabel panjang
+        System.out.println("panjang segi empat: " + panjang);
+        
+    }
+    
+}
